3cdeee598d6c2de25acb5e754982f739b65dfca8
OSUGA-JL <osuga@japan-learning.co.jp>
2026-05-27 14:47:57 +0900

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,5 +1,10 @@
+export const config = {
+  api: {
+    bodyParser: true,
+  },
+};
+
 export default async function handler(req, res) {
-  /* CORSヘッダー */
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');

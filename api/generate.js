686b2928b8779e4c5f7d17d9c465c1d6eaac8491
OSUGA-JL <osuga@japan-learning.co.jp>
2026-05-29 13:58:03 +0900

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,4 +1,4 @@
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -15,10 +15,9 @@ module.exports = async function handler(req, res) {
       },
       body: JSON.stringify(req.body)
     });
-
     const data = await response.json();
     return res.status(response.status).json(data);
   } catch (err) {
     return res.status(500).json({ error: 'API error', detail: err.message });
   }
-};
+}
